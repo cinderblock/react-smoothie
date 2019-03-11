7a36fd983e369ea09199fe3205854b983951eca7
Cameron Tacklind <cameron@tacklind.com>
2019-03-11 01:05:28 -0700

    Rename

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './test.jsx',
+  entry: './example.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'test'),
